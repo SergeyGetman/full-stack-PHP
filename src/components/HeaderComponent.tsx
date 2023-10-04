a48a57cbd46bc4edf9092b921b421106e472daa4
Sergii Getman <sergeygetman2020@gmail.com>
2023-10-04 20:24:58 +0300

    del garbage

--- src/components/HeaderComponent.tsx
+++ src/components/HeaderComponent.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { HeaderStyleComponent } from "../layout/styled-component/Header.style";
+import { Box } from "@mui/material";
 
 const HeaderComponent = () => {
   return (
     <>
       <HeaderStyleComponent>
-        <h1>Welcome header</h1>
+        <Box>...</Box>
       </HeaderStyleComponent>
     </>
   );
